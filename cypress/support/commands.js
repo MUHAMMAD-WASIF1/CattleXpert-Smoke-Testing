be1d9fb2025cff19f92b06f908a1cd0e306b15c2
haroonasif17 <haroon.asif@orangedatacloud.us>
2025-04-15 21:27:46 +0500

    update charge entry

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// common.js
+module.exports = {
+  exampleFunction: () => {
+    console.log("This is a placeholder for common.js");
+  },
+};
