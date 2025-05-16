e7e5333b25b0fd942786838f94df689859bd5a09
haroonasif17 <haroon.asif@orangedatacloud.us>
2025-05-16 18:51:01 +0500

    New reports adding

--- cypress/e2e/Reports/Animal_Health/LotAnalysisByDate.cy.js
+++ cypress/e2e/Reports/Animal_Health/LotAnalysisByDate.cy.js
@@ -20,12 +20,14 @@ describe("LOGIN TO CATTLEXPERT WEBSITE", () => {
     cy.wait(2000);
 
     // Select the "Analysis Date"
-    cy.get('#ctl00_ctl00_ContentPlaceHolder_RightSectionPlaceHolder_ReportViewer1_ctl04_ctl04_txtValue')
-    .type("04/14/2025");
-   
+    cy.get(
+      "#ctl00_ctl00_ContentPlaceHolder_RightSectionPlaceHolder_ReportViewer1_ctl04_ctl04_txtValue"
+    ).type("04/14/2025");
+
     // Click the "View Report" button
-    cy.get('#ctl00_ctl00_ContentPlaceHolder_RightSectionPlaceHolder_ReportViewer1_ctl04_ctl00')
-    .click();
+    cy.get(
+      "#ctl00_ctl00_ContentPlaceHolder_RightSectionPlaceHolder_ReportViewer1_ctl04_ctl00"
+    ).click();
 
     // Wait for the report to load
     cy.wait(5000);
@@ -42,27 +44,66 @@ describe("LOGIN TO CATTLEXPERT WEBSITE", () => {
     cy.contains("Dead").should("exist");
     cy.contains("Buller").should("exist");
     cy.contains("Realizer").should("exist");
-    
 
-    // // Store expected values
-    // const expectedData = {
-    //   From_Pen: "0002",
-    //   To_Pen: "0002",
-    //   Lot: "TL24-2",
-    // };
+    // Store expected values
+    const expectedData = {
+      Pay_Wt: "600",
+     Head_Count: "100",
+      Dead: "0",
+      Realizers: "0"
+    };
 
-    // //Navigate to Health module and Animal Details screen
-    // cy.get(nav.Health).click();
-    // cy.get(nav.Health_Animal_Deatail).click();
+    //Navigate to Processing module and Lot Details screen   
+    cy.get(nav.Processing).click();
+    cy.get(nav.Lot_Details).click();
+    cy.wait(4000); 
 
-    // //Enter the Animal ID in the search box
-    // cy.get("#txtAnimal").type("ASDFGHJ-1", { force: true });
-    // cy.get("#txtPayWeight").click();
+    // Type Lot Number in the dropdown
+    cy.get(".ui-button").type("ANH01"); // Type the value
+    cy.get(".ui-menu-item").contains("ANH01").click();
+    cy.wait(3000);
 
-    // //Verify the data of the report in the Animal Details screen
+    // Verify the Pay_Wt
+    cy.contains(expectedData.Pay_Wt).should("exist");
+    // Navigate to the Summary tab screen
+    cy.get("#btnSummary").click();
 
-    // cy.contains(expectedData.From_Pen).should("exist");
-    // cy.contains(expectedData.To_Pen).should("exist");
-    // cy.contains(expectedData.Lot).should("exist");
+    // Wait for the report to load
+    cy.wait(2000);
+
+    // Step 1: Select "Specify Dates" option
+    cy.get('[value="S"]').click(); // Select the "Specify Dates" radio button
+
+    // Step 2: Set the "From" date
+    cy.get("#txtLDSFromDate") // Replace with the actual selector for the "From" date field
+      .clear() // Clear any existing value
+      .type("04/14/2025"); // Enter the start date
+
+    // Step 3: Set the "To" date
+    cy.get("#txtLDSToDate") // Replace with the actual selector for the "To" date field
+      .clear() // Clear any existing value
+      .type("04/14/2025"); // Enter the end date
+
+    //verify the Head Count from the  Summary tab
+    cy.get('#lblHeadDays').click();
+
+     //verify the Data from the Lot details screen
+    cy.contains(expectedData.Head_Count).should("be.visible");
+    cy.contains(expectedData.Dead).should("be.visible");
+    cy.contains(expectedData.Realizers).should("be.visible");
+
+    // Wait for the report to load
+    cy.wait(3000);
+
+    //Close the summary tab
+    cy.get(
+      ".ui-dialog-titlebar > .ui-button > .ui-button-icon-primary"
+    ).click();
+
+    // Wait for the report to load
+    cy.wait(2000);
+
+   
   });
+    
 });
